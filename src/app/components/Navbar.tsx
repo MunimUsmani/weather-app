fc79e7aa4972bf850d159d4ce6bb8fc558d4436b
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2024-08-25 21:46:09 -0700

    abc

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div></div>
+      Navbar
+    </nav>
+  );
+};
